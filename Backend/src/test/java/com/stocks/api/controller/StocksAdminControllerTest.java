9f196e077debc414dfa098ce54afe5c2d8852d02
Prabhat Ranjan <prabhatr@amazon.com>
2018-03-11 19:36:09 +0100

    More unit tests

--- Backend/src/test/java/com/stocks/api/controller/StocksAdminControllerTest.java
+++ Backend/src/test/java/com/stocks/api/controller/StocksAdminControllerTest.java
@@ -28,7 +28,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -50,7 +49,6 @@ public class StocksAdminControllerTest {
                 .standaloneSetup(new StocksAdminController(stockManager))
                 .setControllerAdvice(new ApiErrorHandler())
                 .build();
-
     }
 
     @Test
--- Backend/src/test/java/com/stocks/api/dal/InMemoryStockDalTest.java
+++ Backend/src/test/java/com/stocks/api/dal/InMemoryStockDalTest.java
@@ -8,10 +8,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import static junit.framework.TestCase.assertNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Tests {@link InMemoryStockDal}.
@@ -59,5 +62,28 @@ public class InMemoryStockDalTest {
 
         assertEquals(3, stockList.get("maxPage"));
         assertEquals(1, stockList.get("page"));
+    }
+
+    @Test
+    public void testPutStock() {
+        final String id = UUID.randomUUID().toString();
+        final String name = "a stock name";
+        final Double price = 20.34;
+        final String symbol = "a symbol";
+        final Stock stock = new Stock()
+                .withId(id)
+                .withName(name)
+                .withPrice(price)
+                .withSymbol(symbol);
+
+        stockDal.putStock(stock);
+
+        final Stock savedStock = stockDal.getById(id);
+
+        assertEquals(id, savedStock.getId());
+        assertEquals(name, savedStock.getName());
+        assertEquals(price, savedStock.getPrice());
+        assertEquals(symbol, savedStock.getSymbol());
+        assertNotNull(stock.getLastUpdateDate());
     }
 }
